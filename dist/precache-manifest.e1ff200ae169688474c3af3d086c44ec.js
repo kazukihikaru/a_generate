a365782b6fdd68b48a64f73ef773ddc27578432c
bepostive <bepostive@abc.com>
2022-03-23 20:39:53 +0800

    release

---
+++ dist/precache-manifest.e1ff200ae169688474c3af3d086c44ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10156c7875d8bffed501",
+    "url": "/aicd_generate/css/app.20a9bb2e.css"
+  },
+  {
+    "revision": "4176ed24e7f31447f0ab",
+    "url": "/aicd_generate/css/chunk-vendors.5cbbb097.css"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/aicd_generate/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/aicd_generate/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/aicd_generate/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/aicd_generate/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "cf713ac89bea4cec6988ea436d5f7280",
+    "url": "/aicd_generate/img/title.cf713ac8.png"
+  },
+  {
+    "revision": "36d47ff90cb6178b63ee11ace803dbf9",
+    "url": "/aicd_generate/index.html"
+  },
+  {
+    "revision": "e4d54938f3b9a5411579",
+    "url": "/aicd_generate/js/about.00cfaf4c.js"
+  },
+  {
+    "revision": "10156c7875d8bffed501",
+    "url": "/aicd_generate/js/app.5950a704.js"
+  },
+  {
+    "revision": "4176ed24e7f31447f0ab",
+    "url": "/aicd_generate/js/chunk-vendors.8c9e5abb.js"
+  },
+  {
+    "revision": "ed1465aa8126978d74a9ab8e01c84f6a",
+    "url": "/aicd_generate/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/aicd_generate/robots.txt"
+  }
+]);
